ac3c094038c313102f5d425ed41835b0b8bcaec9
Toni-Ty <antonique2479@yahoo.com>
2020-04-09 23:41:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function writeCards(Array3s, surprise) {
 for (let i = 0; i < Array3s.length; i++) {
 const newArray = (`Thank you, ${Array3s[i]}, for the wonderful ${surprise} gift!`);
  }
-return newArray;
-}
+return newArray
+
