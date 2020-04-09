fb73ff0dab08198f685c50bd6a744f5b2765007a
Toni-Ty <antonique2479@yahoo.com>
2020-04-09 23:12:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 const names =  [ 'Lisa', 'Kaitlin', 'Jan' ];
 
 function writeCards(names, event) {
-
+for (let i = 0; i < names.length; i++) {
+  console.log(`Thank you, ${name}, for the wonderful ${event} gift!`);
+}
 }
