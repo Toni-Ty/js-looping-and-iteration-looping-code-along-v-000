c10b0b846199db5b499f87cce9b320d1d7c1e62f
Toni-Ty <antonique2479@yahoo.com>
2020-04-09 23:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
 const Array3 =  [ 'Lisa', 'Kaitlin', 'Jan'];
 
-function writeCards(Array3) {
+function writeCards(Array3, surprise) {
 for (let i = 0; i < Array3s.length; i++) {
-  console.log(`Thank you, ${Array3[i]}, for the wonderful surprise gift!`);
+  console.log(`Thank you, ${Array3[i]}, for the wonderful ${surprise} gift!`);
  }
 return Array3;
 }
