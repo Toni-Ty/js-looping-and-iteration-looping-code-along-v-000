f9efc49b310361e3c0f16c2a8ece3a85ec387cea
Toni-Ty <antonique2479@yahoo.com>
2020-04-09 23:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Array3s =  [ 'Lisa', 'Kaitlin', 'Jan'];
 
 function writeCards(Array3s, surprise) {
 for (let i = 0; i < Array3s.length; i++) {
-const newArray = `Thank you, ${Array3s[i]}, for the wonderful ${surprise} gift!`);
+const newArray = (`Thank you, ${Array3s[i]}, for the wonderful ${surprise} gift!`);
  }
 return newArray;
 }
